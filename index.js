3c9c3146d29690aa5ca3fc4c017924947e91993c
meganr <meganrm@gmail.com>
2017-10-10 18:07:50 -0700

    edited readme

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ const http = require('http');
 const url = require('url');
 const urlParser = require('./lib/urlParser');
 
-const server = http.createServer((req, res) => {
-  req.url = url.parse(req.url);
 
-  if (req.method === 'GET') {
 
-    urlParser.getRouter(req, res);
+function start(){
+  const server = http.createServer((req, res) => {
+    req.url = url.parse(req.url);
 
-  } else if (req.method === 'POST') {
+    if (req.method === 'GET') {
 
-    if (req.url.pathname.startsWith('/api')) {
-      urlParser.postRouter(req, res);
+      urlParser.getRouter(req, res);
+
+    } else if (req.method === 'POST') {
+
+      if (req.url.pathname.startsWith('/api')) {
+        urlParser.postRouter(req, res);
+      } else {
+        urlParser.sendResponse(res, 400, 'bad request');
+      }
+
     } else {
+
       urlParser.sendResponse(res, 400, 'bad request');
+
     }
-
-  } else {
-
-    urlParser.sendResponse(res, 400, 'bad request');
-
-  }
-});
-
-
-server.listen(3000);
+  });
+  server.listen(3000);
+}
