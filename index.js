566fa531e15cd8c64e94aef93694ac903c20a72f
meganr <meganrm@gmail.com>
2017-10-10 17:35:50 -0700

    moved code to parser

--- index.js
+++ index.js
@@ -2,49 +2,53 @@
 
 const http = require('http');
 const url = require('url');
+const urlParser = require('./lib/urlParser');
 const bodyParser = require('./lib/bodyParser');
 
 let sendResponse = function(res, status, body, textType) {
-  res.writeHead(status, {'Content-Type': `text/${textType}`});
+  res.writeHead(status, {'Content-Type': `${textType ? textType : 'text/plain'}`});
   res.write(body);
   res.end();
 };
 
 const server = http.createServer((req, res) => {
-  //things coming in from the header will be automatically stored on the req object
-  //shouldnt have any data in get request
-  //if you want to send data, put in the header
   req.url = url.parse(req.url);
-  if (req.method === 'GET' && req.url.pathname === '/') {
-    let statusCode = 200;
-    sendResponse(res, statusCode, bodyParser.mainPage(), 'html');
-  }
-  else if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-    //handeling query params
-    sendResponse(res, 200, bodyParser.makePage('cow', req.url.query), 'html');
-  }
-  //parse data
-  else if (req.method === 'POST' && req.url === '/') {
+  if (req.method === 'GET') {
+
+    urlParser.getRouter(req, res);
+
+  } else if (req.method === 'POST' && req.url.pathname === '/api/cowsay') {
     let body = '';
     req.on('data', function(data){
       //there is a max buffer size so we have to add chunks together
       body += data.toString();
     });
     req.on('end', function(){
-      console.log(body);
       let json;
+      let returnBody = {};
+
       try {
+
         json = JSON.parse(body);
-        console.log(json);
-      } catch(e){
-        return sendResponse(res, 400, 'bad JSON :()');
+
+      } catch(e) {
+
+        returnBody['error'] = 'invalid request: body required'
+        return sendResponse(res, 400, returnBody);
+
       }
-      sendResponse(res, 200, 'got the data, thanks!');
+
+      let dataValue = urlParser.checkJSON(json, 'text');
+      if (dataValue) {
+        returnBody['content'] = `cowsay cow ${dataValue}`;
+      } else {
+        returnBody['error'] = 'invalid request: text query required';
+      }
+      sendResponse(res, 200, JSON.stringify(returnBody), 'plain/text');
     });
   } else {
     sendResponse(res, 400, 'bad request');
   }
-
 });
 //request is an event emitter, response is not.
 
